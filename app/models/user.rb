611c7f597d94f846058a6fba5e3b0064673bad07
Dev Patel <dhpatel15@gmail.com>
2019-07-24 13:20:48 -0400

    added assoication to user class

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
 	has_many :comments
-	has_many :votes, as: :votable 
+	has_many :user_votes, as: :votable 
+	has_many :comment_votes, as: :votable, :class_name => 'Vote'
 end
